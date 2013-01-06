ced24c31783032714d9f3a391b27c6ef1c1d735d
Dominik Bonsch <dominik.bonsch@webfrap.net>
2013-01-06 16:00:57 +0100

    removed needless whitespace

--- js_src/wgt/wcm/ui/Dropform.js
+++ js_src/wgt/wcm/ui/Dropform.js
@@ -6,36 +6,36 @@
 $R.addAction( 'ui_dropform', function( jNode ){
 
   var source = jNode.find( 'var' );
-  
+
   var props  = source.is( 'var' )? $WGT.robustParseJSON( source.text() ) : {};
-  
-  
+
+
   var menuWidth = 750,
     menuItems = [],
     overlayData,
     nextNode,
     nodeId;
-  
+
   if( source.length > 0 ){
-    
+
     jNode.click( function( event ){
-      
+
       nextNode = jNode.next();
       nodeId   = jNode.attr('id');
-      
+
       overlayData = jNode.data( 'mini-menu-overlay' );
-      
+
       if( !overlayData && nextNode.is( '.'+nodeId ) ){
         overlayData = nextNode.html();
         jNode.data( 'mini-menu-overlay', overlayData );
       }
-      
+
       // wenn nicht vorhanden vom server laden
       if( !overlayData ){  //!nextNode.is( '.'+nodeId ) ){
 
         var theTemplate = $R.get( props.url+'&input='+nodeId ).data;
         jNode.data( 'mini-menu-overlay', theTemplate );
-        
+
         //var theContentNode = $S( '.'+nodeId );
         var theContent = $S( '<div>'+theTemplate+'</div>' );
 
@@ -55,39 +55,37 @@ $R.addAction( 'ui_dropform', function( jNode ){
           overlayStyle: {width:menuWidth+'px'},
           menuItems   : menuItems
         });
-        
+
         jNode.click();
       }
-      
-      event.stopPropagation();
-          
+
     });
-    
+
   }
   else{
-    
+
     nextNode = jNode.next();
     nodeId   = jNode.attr('id');
-    
+
     overlayData = jNode.data( 'mini-menu-overlay' );
-    
+
     if( !overlayData && nextNode.is( '.'+nodeId ) ){
       overlayData = nextNode.html();
       jNode.data( 'mini-menu-overlay', overlayData );
       nextNode.remove();
     }
-      
+
     //var theContentNode = $S( '.'+jNode.attr('id') );
     var theContent = $S('<div>'+overlayData+'</div>');
-    
+
     theContent.find(':input').removeClass('flag-template');
-  
+
     ///TODO add i from search form to make order persistent
     menuItems.push({
       type    : 'html',
       content : theContent.html()
     });
-  
+
     jNode.miniMenu({
       button      : 'Close',
       plain 	    : false,
@@ -95,9 +93,9 @@ $R.addAction( 'ui_dropform', function( jNode ){
       overlayStyle:{width:menuWidth+'px'},
       menuItems   : menuItems
     });
-    
+
   }
-  
+
   jNode.removeClass('wcm_ui_dropform');
-  
+
 });
