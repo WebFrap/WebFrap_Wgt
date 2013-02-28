c81577db837cc93ef8de7f26bca9db8a7b55e86a
Dominik Bonsch <dominik.bonsch@webfrap.net>
2013-02-28 17:16:42 +0100

    added PUT / POST selection

--- js_src/wgt/wcm/action/Post.js
+++ js_src/wgt/wcm/action/Post.js
@@ -4,30 +4,50 @@
  * @author dominik alexander bonsch <db@webfrap.net>
  */
 $R.addAction( 'req_post', function( jNode ){
-  
+
   if( $C.DEBUG.WCM.ACTION )
     console.log( 'wcm req_post path: ' +jNode.getNodePath('/')  );
-  
+
   if(!(jNode.is('a')||jNode.is('button')))
     return;
-  
+
   if( jNode.is('a') ){
 
     jNode.click( function(){
-      
+
       $R.post(this.href+"&request=ajax");
       return false;
     });
-    
+
   }
   else{
-    
+
     jNode.click(function(){
       $R.post( jNode.attr("value")+"&request=ajax" );
       return false;
     });
-  }  
+  }
 
   jNode.removeClass("wcm_req_post");
+
+});
+
+$R.addAction( 'req_post_selection', function( jNode ){
+
+  jNode.click(function(){
+
+    var postHref = this.href;
+
+
+    $S( jNode.attr( 'wgt_elem' ) ).find( 'tr.wgt-selected' ).each(function(){
+      postHref += '&slct[]='+$S(this).attr('wgt_eid');
+    });
+
+    $R.post(postHref+"&request=ajax",{});
+
+    return false;
+
+  });
+  jNode.removeClass("wcm_req_post_selection");
 
 });
--- js_src/wgt/wcm/action/Put.js
+++ js_src/wgt/wcm/action/Put.js
@@ -7,27 +7,47 @@ $R.addAction( 'req_put', function( jNode ){
 
   if( $C.DEBUG.WCM.ACTION )
     console.log( 'wcm req_put path: ' +jNode.getNodePath('/')  );
-  
+
   if( !(jNode.is('a')||jNode.is('button')) )
     return;
-  
+
   if( jNode.is('a') ){
 
     jNode.click( function(){
-      
+
       $R.put(this.href+"&request=ajax", {} );
       return false;
     });
-    
+
   }
   else{
-    
+
     jNode.click(function(){
       $R.put( jNode.attr("value")+"&request=ajax", {} );
       return false;
     });
-  }  
+  }
 
   jNode.removeClass("wcm_req_put");
+
+});
+
+
+$R.addAction( 'req_put_selection', function( jNode ){
+
+  jNode.click(function(){
+
+    var putHref = this.href;
+
+    $S( jNode.attr( 'wgt_elem' ) ).find( 'tr.wgt-selected' ).each(function(){
+      putHref += '&slct[]='+$S(this).attr('wgt_eid');
+    });
+
+    $R.put(putHref+"&request=ajax",{});
+
+    return false;
+
+  });
+  jNode.removeClass("wcm_req_put_selection");
 
 });
