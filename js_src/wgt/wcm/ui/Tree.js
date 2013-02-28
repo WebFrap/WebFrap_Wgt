481ce702afab76a8683e3ec4fb5d181d4aedc580
Dominik Bonsch <dominik.bonsch@webfrap.net>
2013-02-28 14:26:52 +0100

    implemented links

--- js_src/wgt/wcm/ui/Tree.js
+++ js_src/wgt/wcm/ui/Tree.js
@@ -9,7 +9,7 @@ $R.addAction( 'ui_tree', function( jNode ){
 
   var settings = {},
     cfgData = jNode.find('var#'+jNode.attr('id')+'-cfg-tree');
-  
+
   settings = cfgData.is('var')
     ? $WGT.robustParseJSON(cfgData.text())
     : {};
@@ -17,9 +17,24 @@ $R.addAction( 'ui_tree', function( jNode ){
   settings.imagePath = $C.imagePath+'wgt/';
 
   console.log('tree '+jNode.attr('id'));
-  
-  jNode.dynatree( settings ).removeClass('wcm_ui_tree');
 
+  settings.onActivate = function(node) {
+    // Use <a> href and target attributes to load the content:
+    if( node.data.href ){
+
+      if( !$S(node).is('.extern') ){
+
+        $R.get(node.data.href);
+
+      } else {
+
+        window.open(node.data.href, node.data.target);
+      }
+
+    }
+  };
+
+  jNode.dynatree( settings ).removeClass('wcm_ui_tree');
 
 });
 
@@ -28,13 +43,13 @@ $R.addAction( 'ui_json_tree', function( jNode ){
   var settings = {},
     cfgData = jNode.find('var.tree-cfg'),
     treeData = jNode.find('var.data');
-  
+
   settings = cfgData.is('var')
     ? $WGT.robustParseJSON(cfgData.text())
     : {};
-    
+
   settings.children = $WGT.robustParseJSON( treeData.text() );
-  
+
   settings.imagePath = $C.imagePath+'wgt/';
 
   jNode.dynatree( settings ).removeClass('wcm_ui_json_tree');
