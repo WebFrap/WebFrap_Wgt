35f67bc6591bfa6cb8af0a3eadd0a3d1ea62e2a1
Dominik Bonsch <dominik.bonsch@webfrap.net>
2013-03-10 11:19:04 +0100

    removed some metadata

--- js_src/wgt/wcm/graph/AreaChart.js
+++ js_src/wgt/wcm/graph/AreaChart.js
@@ -1,9 +1,6 @@
 /* jshint forin:true, noarg:true, noempty:true, eqeqeq:true, bitwise:true, strict:true, undef:true, unused:true, curly:true, browser:true, devel:true, jquery:true, indent:4, maxerr:50 */
 /*
  * WGT Web Gui Toolkit
- *
- * Copyright (c) 2009 webfrap.net
- *
  * http://webfrap.net/WGT
  *
  * @author Dominik Bonsch <db@webfrap.net>
