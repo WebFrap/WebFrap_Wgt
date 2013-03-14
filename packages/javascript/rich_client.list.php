83aa99dd8afd842704a9e26a3a402bb784b96e63
Dominik Bonsch <dominik.bonsch@webfrap.net>
2013-03-15 00:04:34 +0100

    added stacked bar chart

--- packages/javascript/rich_client.list.php
+++ packages/javascript/rich_client.list.php
@@ -170,6 +170,7 @@ $files = array
   PATH_WGT.'js_src/wgt/wcm/graph/BivariateArea.js',
   PATH_WGT.'js_src/wgt/wcm/graph/BulletChart.js',
   PATH_WGT.'js_src/wgt/wcm/graph/StackedAreaChart.js',
+  PATH_WGT.'js_src/wgt/wcm/graph/StackedBarChart.js',
   PATH_WGT.'js_src/wgt/wcm/graph/AreaChart.js',
   PATH_WGT.'js_src/wgt/wcm/graph/DonutChart.js',
   PATH_WGT.'js_src/wgt/wcm/graph/PieChart.js',
