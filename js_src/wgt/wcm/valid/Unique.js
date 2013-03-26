105eccfb991ff5706441226d0b4b64494bbb25fe
Dominik Bonsch <dominik.bonsch@webfrap.net>
2013-03-26 14:50:44 +0100

    added uniqueness check

---
+++ js_src/wgt/wcm/valid/Unique.js
@@ -0,0 +1,41 @@
+/* Licence see: /LICENCES/wgt/licence.txt */
+
+/**
+ * Uniqueness check
+ * 
+ * @author dominik bonsch <db@webfrap.net>
+ */
+$R.addAction( 'valid_unique', function( jNode ){
+
+  jNode.removeClass("wcm_unique");
+  
+  // nur auf textarea und inputs verwenden
+  // bei selectboxen wäre das vermutlich bescheiden
+  if( jNode.is('input') || jNode.is('textarea') ){
+
+    if( false == $R.get(jNode.attr('wgt_cksrv')) ){
+      jNode.addClass( 'state-warn' );
+      jNode.attr('title',"Value allready exists");
+    }
+    else{
+      jNode.addClass( 'state-ok' );
+    }
+    
+    jNode.bind( 'change.valid_unqiue', function(){
+      if( '' == ''+jNode.val() ){
+        jNode.removeClass( 'state-ok' );
+        jNode.addClass( 'state-warn' );
+        jNode.attr('title',"Value allready exists");
+      }
+      else{
+        jNode.removeClass( 'state-warn' );
+        jNode.addClass( 'state-ok' );
+        jNode.attr('title',"Insert value");
+      }
+        
+    });
+  
+  }
+  
+
+});
