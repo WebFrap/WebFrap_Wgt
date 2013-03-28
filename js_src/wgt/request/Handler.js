eee9e261f05991a86174ba588fbb7ceaacd3fa5b
Dominik Bonsch <dominik.bonsch@webfrap.net>
2013-03-28 11:50:30 +0100

    improved error handling

--- js_src/wgt/request/Handler.js
+++ js_src/wgt/request/Handler.js
@@ -82,7 +82,7 @@
            +' This can happen when your Browser is in the Offline Mode, or when an internal Server Error occures.'
            +' Please report if this problem persists.'
         );
-        console.log( xmlData );
+        console.dir( xmlData );
         return null;
       }
 
