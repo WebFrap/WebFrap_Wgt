1f82d9e8dbecf5c3f9187dd735475a3aa0234497
Dominik Bonsch <dominik.bonsch@webfrap.net>
2013-04-03 17:52:18 +0200

    added search for multiselect

--- js_src/wgt/wcm/ui/MultiSelect.js
+++ js_src/wgt/wcm/ui/MultiSelect.js
@@ -7,14 +7,33 @@ $R.addAction( 'ui_multiselect', function( jNode ){
 
   jNode.removeClass("wcm_ui_multiselect");
   
-  var jNode = $S('#'+jNode.attr('id')),
-    settings = {};
-  
+  var settings = {};
+
   var cfgData = $S('var#'+jNode.attr('id')+'-cfg-mslct');
   settings = cfgData.is('var')
     ? $WGT.robustParseJSON(cfgData.text())
     : {};
 
   jNode.multiselect(settings);
+
+});
+
+$R.addAction( 'ui_multiselect_search', function( jNode ){
+
+  jNode.removeClass("wcm_ui_multiselect_search");
+  var formId = jNode.getActionClass('fparam',true,'-'),
+    settings = {},
+    cfgData = $S('var#'+jNode.attr('id')+'-cfg-mslct');
   
+  settings = cfgData.is('var')
+    ? $WGT.robustParseJSON(cfgData.text())
+    : {
+      'buttonWidth':"96%",
+      close: function(){
+        $R.form( formId, null, {success:function(){ jNode.focus(); }} );
+      }
+    };
+
+  jNode.multiselect(settings);
+
 });
