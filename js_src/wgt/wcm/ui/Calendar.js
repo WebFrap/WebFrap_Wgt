988981fc6073c6b5e1c739e24501a980a0163244
Dominik Bonsch <dominik.bonsch@webfrap.net>
2013-05-03 14:19:17 +0200

    extended the calendar

--- js_src/wgt/wcm/ui/Calendar.js
+++ js_src/wgt/wcm/ui/Calendar.js
@@ -35,56 +35,58 @@ $R.addAction( 'ui_calendar', function( jNode ){
   jNode.removeClass('wcm_ui_calendar');
   
   jNode.appear(function(){
-	  
-	  var dataBody = jNode.next();
-	  
-	  var tmpId = jNode.attr('id');
-	  tmpId = tmpId.substring( 4, tmpId.length );
-	  
-	  var formObj = $S('#wgt-form-'+tmpId); ;
-	  
-	  // var ist der datencontainer der benötigt wird um einen callendar
-	  // mit daten zu befüllen
-	  if( !dataBody.is('var') ){
-	    alert('found no body');
-	    return;
-	  }
-	  
-	  // extrahieren der json daten aus dem request
-	  dataBody = $S.parseJSON( dataBody.text() );
-	
-	  // erstellen des calendar nodes
-	  var calendarObj = jNode.fullCalendar({
-	    
-	      header: {
-	          left: 'prevYear,prev,today,next,nextYear',
-	          center: 'title',
-	          right: 'month,agendaWeek,agendaDay'
-	      },
-	      editable: true,
-	      events: dataBody.data||[], // wenn data nicht existiert einen leeren array übergeben
-	      eventDrop: function(event,dayDelta,minuteDelta,allDay,revertFunc) {
-	        
-	        // Event verschieben
-	        alert(
-	            event.title + " was moved " +
-	            event.start.toString("yyyy-mm-dd") + " new start "
-	        );
-	
-	      },
-	      eventResize: function(event,dayDelta,minuteDelta,revertFunc) {
-	
-	        alert(
-	            event.title + " was resized " +
-	            event.start.toString("yyyy-mm-dd") + " new start "
-	        );
-	
-	      }
-	
-	  });
+    
+    var defSettings = {
+        header: {
+            left: 'prevYear,prev,today,next,nextYear',
+            center: 'title',
+            right: 'month,agendaWeek,agendaDay'
+        },
+        editable: true,
+        eventDrop: function(event,dayDelta,minuteDelta,allDay,revertFunc) {
+          
+          // Event verschieben
+          alert(
+              event.title + " was moved " +
+              event.start.toString("yyyy-mm-dd") + " new start "
+          );
   
-	  // das calendar objekt wird an ein formular gebunden
-	  formObj.data( 'wgt-calendar' , calendarObj );
+        },
+        eventResize: function(event,dayDelta,minuteDelta,revertFunc) {
+        
+          // Event
+          alert(
+              event.title + " was resized " +
+              event.start.toString("yyyy-mm-dd") + " new start "
+          );
+  
+        }
+  
+    };
+    
+	var cfgData = jNode.next(),
+		settings = cfgData.is('var')
+	    ? $WGT.robustParseJSON(cfgData.text())
+	    : {};
+	          
+    settings = $S.extend({}, defSettings, settings);
+    
+    settings.events = function(start, end, callback) {
+    	
+    	var data = $R.get('service.php?c=Webfrap.Calendar.search&start='+Math.round(start.getTime() / 1000)+'&end='+Math.round(end.getTime() / 1000));
+    	callback(data);
+    };
+    
+    var tmpId = jNode.attr('id');
+    tmpId = tmpId.substring( 4, tmpId.length );
+    
+    var formObj = $S('#wgt-form-'+tmpId); ;
+
+    // erstellen des calendar nodes
+    var calendarObj = jNode.fullCalendar(settings);
+  
+    // das calendar objekt wird an ein formular gebunden
+    formObj.data( 'wgt-calendar' , calendarObj );
   });
   
   
