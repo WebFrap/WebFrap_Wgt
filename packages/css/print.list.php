c352fc49c3adf621c7597506d7df0d1fc1f24665
Dominik Bonsch <dominik.bonsch@webfrap.net>
2013-05-10 18:28:02 +0200

    added print packages

---
+++ packages/css/print.list.php
@@ -0,0 +1,28 @@
+<?php
+/*******************************************************************************
+*
+* @author      : Dominik Bonsch <dominik.bonsch@webfrap.net>
+* @date        :
+* @copyright   : Webfrap Developer Network <contact@webfrap.net>
+* @project     : Webfrap Web Frame Application
+* @projectUrl  : http://webfrap.net
+*
+* @licence     : BSD License see: LICENCE/BSD Licence.txt
+*
+* @version: @package_version@  Revision: @package_revision@
+*
+* Changes:
+*
+*******************************************************************************/
+
+echo '@charset "utf-8";'.NL;
+
+$files = array
+(
+  PATH_WGT.'layout/print/print.core.css',
+);
+
+foreach ($files as $file) {
+  include $file;
+  echo NL;
+}
