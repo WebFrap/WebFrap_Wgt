aac959b6daa8b676d6e98941d239a67260803eb6
Dominik Bonsch <dominik.bonsch@webfrap.net>
2013-05-10 18:27:51 +0200

    added nvd3 support

---
+++ packages/javascript/nvd3.list.php
@@ -0,0 +1,24 @@
+<?php
+/*******************************************************************************
+*
+* @author      : Dominik Bonsch <dominik.bonsch@webfrap.net>
+* @date        :
+* @copyright   : Webfrap Developer Network <contact@webfrap.net>
+* @project     : Webfrap Web Frame Application
+* @projectUrl  : http://webfrap.net
+*
+* @licence     : BSD License see: LICENCE/BSD Licence.txt
+*
+* @version: @package_version@  Revision: @package_revision@
+*
+* Changes:
+*
+*******************************************************************************/
+
+// ist schon minified
+$minify = false;
+
+$files = array(
+  PATH_WGT.'js_src/vendor/nvd3/nv.d3.min.js',
+);
+
